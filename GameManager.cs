b5e8d19f8f226e2af303db3d4a114be275dc5f4d
wny0320 <33829442+wny0320@users.noreply.github.com>
2022-11-13 18:02:05 +0900

    Making Defence Game
    interface and Player Making, Not complete yet

---
+++ GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int Resource;
+    void Start()
+    {
+        Resource = 30;
+    }
+
+    void Update()
+    {
+        
+    }
+}
